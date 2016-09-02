f7beda9733c1c84f30fc6a1627132e93e180f76f
EmilyClaudet <emily@kano.me>
2016-09-03 01:13:59 +0200

    Initial commit

---
+++ src/main.lua
@@ -0,0 +1,15 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.keyreleased(key)
+end
+
+function love.draw()
+end
+
+function love.quit()
+    print('Quitting Chasing Game...')
+end
