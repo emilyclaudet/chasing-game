34b1700d7d027399fe7a357e747f3979ad164e89
EmilyClaudet <emily@kano.me>
2016-11-26 19:49:30 +0100

    merge attempt

--- src/main.lua
+++ src/main.lua
@@ -1,40 +1,20 @@
+require 'post'
+require 'player'
+
 platform = {}
-player = {}
-posts = {}
 
 function love.load()
 	state = 'play'
 	jumpSound = love.audio.newSource('res/jumpSound.wav', 'static' )
 	loseSound = love.audio.newSource('res/loseSound.wav', 'static' )
-	playerX = 32
-	playerY = 416
-	playerSpeed = 200
-	player = love.graphics.newImage("res/player.png")
-	playerSprite = love.graphics.newQuad(0, 32, 32, 32, player:getDimensions())
-
+	scoreSound = love.audio.newSource('res/scoreIncrease.wav', 'static' )
 	platform.width = love.graphics.getWidth()
 	platform.height = love.graphics.getHeight() 
 	platform.x = 0                              
 	platform.y = platform.height / 1.34
-
-	playerGround = playerY     -- This makes the character land on the plaform.
-	playerYVelocity = 0
-	playerJumpHeight = -350
-	playerGravity = -1000
-
-	gap = 200
-	postWidth = 28
-	postHeight = 48
-	postVelocity = 200
-	postX = love.graphics.getWidth()
-	postY = 416 - postHeight/2
-
-	postWidth2 = 28
-	postHeight2 = 48
-	postVelocity2 = 200
-	postX2 = love.graphics.getWidth() + gap
-	postY2 = 416 - postHeight/2
-
+	score = 0
+	load_player()
+	load_post()
 end
 
 function love.update(dt)
@@ -42,63 +22,23 @@ function love.update(dt)
 		return 
 	end
 
-	if love.keyboard.isDown('left') then
-		playerX = playerX - (playerSpeed * dt)
+	update_player(dt)
+    check_player_on_ground(dt)
+	regenerate_post(dt)
+	limit_player(dt)
+	-- Score increases everytime a successful jump
+	if postX < 0 then
+		score = score + 1
+		scoreSound:play()
 	end
-
-	if love.keyboard.isDown('right') then
-		playerX = playerX + (playerSpeed * dt)
-	end
-
-	if love.keyboard.isDown('space') then
-		if  playerYVelocity == 0 then					-- Check player is on the ground
-			playerYVelocity = playerJumpHeight		-- The player's Y-Axis Velocity is set to it's Jump Height.
-			jumpSound:play()
-		end
-	end
-
-    if playerYVelocity ~= 0 then                                      -- The game checks if player has "jumped" and left the ground.
-		playerY = playerY + playerYVelocity * dt                -- This makes the character ascend/jump.
-		playerYVelocity = playerYVelocity - playerGravity * dt -- This applies the gravity to the character.
-	end
- 
-    -- This is in charge of collision, making sure that the character lands on the ground.
-    if playerY > playerGround then    -- Check if player jumps
-		playerYVelocity = 0
-    	playerY = playerGround 
-	end
-
-	-- Controls player's range to not walk off edge of screen
-	if playerX < 0 then
-		playerX = 0
-	end
-	if playerX > love.graphics.getWidth() / 2 - 32 then
-		playerX = love.graphics.getWidth() / 2 - 32
-	end
-	
-	elapsedTime = 0 
-	elapsedTime = elapsedTime + dt
-
-	if elapsedTime > 0 then
-		if postX < 0 then
-			postX = love.graphics.getWidth()
-		else
-			postX = postX - postVelocity * dt	
-		end
-	end
-
-	if elapsedTime > 0 then
-		if postX2 < 0 then
-			postX2 = love.graphics.getWidth()
-		else
-			postX2 = postX2 - postVelocity2 * dt	
-		end
-	end
-
 	-- Make player die if they hit the post
 	if check_collision_post1() == true or check_collision_post2() == true then
 		loseSound:play()
 		state = 'lose'
+	end
+
+	if score == 2 then
+		state = 'win'
 	end
 end
 
@@ -113,20 +53,19 @@ function love.draw()
 	love.graphics.setColor(0, 255, 255)
 	love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 	-- Player
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.draw(player, playerSprite, playerX, playerY)
+	draw_player()
 	-- Ground
 	love.graphics.setColor(255, 255, 255)
 	love.graphics.rectangle('fill', platform.x, platform.y, platform.width, platform.height)
-	-- Post 1
-	love.graphics.setColor(255, 0, 0)
-	love.graphics.rectangle('fill', postX, postY, postWidth, postHeight)
-	-- Post 2
-	love.graphics.setColor(255, 0, 0)
-	love.graphics.rectangle('fill', postX2, postY2, postWidth2, postHeight2)
-
+	-- Posts
+	draw_post()
+	-- Score
+	love.graphics.print(score, 10, 20, 0, 1, 1)
 	if state == 'lose' then
 		draw_lose_screen()
+	end
+	if state == 'win' then
+		draw_win_screen()
 	end
 end
 
@@ -135,8 +74,13 @@ function draw_lose_screen()
 	love.graphics.print("You died", 10, 10, 0, 1, 1)
 end
 
--- Returns true if player overlaps with post 1
-function check_collision_post1(x1,y1,w1,h1,x2,y2,w2,h2)
+function draw_win_screen()
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.print("YOU WIN", 100, 100, 0, 2, 2)
+end
+
+-- Returns true if player overlaps with post
+function check_collision(x1,y1,w1,h1,x2,y2,w2,h2)
 	x1 = playerX - 2
 	y1 = playerY - 2
 	w1 = 28
---
+++ src/post.lua
@@ -0,0 +1,27 @@
+posts = {}
+
+function load_post()
+	postWidth = 28
+	postHeight = 48
+	postVelocity = 200
+	postX = love.graphics.getWidth()
+	postY = 416 - postHeight/2
+end
+
+function regenerate_post(dt)
+	elapsedTime = 0 
+	elapsedTime = elapsedTime + dt
+
+	if elapsedTime > 0 then
+		if postX < 0 then
+			postX = love.graphics.getWidth()
+		else
+			postX = postX - postVelocity * dt	
+		end
+	end
+end
+
+function draw_post()
+	love.graphics.setColor(255, 0, 0)
+	love.graphics.rectangle('fill', postX, postY, postWidth, postHeight)
+end
